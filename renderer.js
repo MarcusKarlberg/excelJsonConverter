9e48a165f4ccb4c0d64980c10f1903d60368ae66
MarcusKarlberg <marcus.karlberg@hotmail.com>
2020-12-26 23:20:44 +0100

    first commit

---
+++ renderer.js
@@ -0,0 +1,44 @@
+'use strict';
+const { remote } = require('electron');
+const excelToJson = require('convert-excel-to-json');
+const fs = require('fs');
+const {dialog} = require('electron').remote;
+
+function convertXlsToJson(path) {
+    console.log('converting...' + path);
+    return excelToJson({
+        sourceFile: path
+    });
+}
+
+function writeToFile(filePath, jsonContent) {
+    fs.writeFile(filePath.concat('requirements.json'), jsonContent, function (err) {
+        if (err) return console.log(err);
+        console.log('writing DONE!');
+    });
+}
+
+function showResult(path) {
+    dialog.showOpenDialog({
+        defaultPath: path,
+        properties: ['openDirectory', 'multiSelections']
+    }, function (files) {
+        if (files !== undefined) {
+           return;
+        }
+    });
+}
+
+async function convert() {
+    var fileData = document.querySelector('#file_input').files[0];
+    var pathOnly = fileData.path.replace(fileData.name,'');
+    const result = await convertXlsToJson(fileData.path);
+    console.log('conversion DONE!');
+    console.log('writing to file...');
+    await writeToFile(pathOnly, JSON.stringify(result));
+    showResult(pathOnly);
+}
+
+document.querySelector('#convert').addEventListener('click', () => {
+    convert();
+});
